308bf1ca84d09d400fa4b07e31af860c9f4e049c
Gabriel <40807192+SkynoDore@users.noreply.github.com>
2025-09-17 00:30:49 +0200

    added error messages

--- src/js/components/Search.jsx
+++ src/js/components/Search.jsx
@@ -1,90 +1,101 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BookCard from "./BookCard";
 import { useDebounce } from "./hooks/useDebounce";
 
 export default function Search() {
   const [query, setQuery] = useState("");
   const [books, setBooks] = useState([]);
-  const debouncedQuery = useDebounce(query, 500);
+  const [errorMsg, setError] = useState("");
 
   const handleSearch = async (e) => {
-    try{
-    e.preventDefault();
-    const res = await fetch(
-      `https://www.googleapis.com/books/v1/volumes?q=${debouncedQuery}`
-    );
-    const data = await res.json();
-    setBooks(data.items || []);
+    try {
+      e.preventDefault();
+      const res = await fetch(
+        `https://www.googleapis.com/books/v1/volumes?q=${query}`
+      );
+      if (!res.ok) {
+        let message = `Error ${res.status}: ${res.statusText}`;
+        if (res.status === 400) {
+          message +=
+            " — La búsqueda no puede estar vacía o tiene caracteres no válidos.";
+        }
+        throw new Error(message);
+      }
+      const data = await res.json();
+      setBooks(data.items || []);
+      setError("");
+    } catch (error) {
+      setError(error.message);
     }
-    catch (error){
-      console.error(error)
-    }
-    /*await fetch('/api/searches', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-        },
-        body: JSON.stringify({ term: query }),
-    });*/
   };
 
   return (
     <>
       <h1 className="mt-4">Bookbuzz</h1>
       <div className="d-flex flex-column align-items-center justify-content-center col-12 col-md-6 p-2">
-        <form className="d-flex items-center w-100 gap-3" onSubmit={handleSearch}>
+        <form
+          className="d-flex items-center w-100 gap-3"
+          onSubmit={handleSearch}
+        >
           <input
             type="text"
             value={query}
-            onChange={(e) => setQuery(e.target.value)}
+            onChange={(e) => {
+              setQuery(e.target.value);
+            }}
             placeholder="Buscar libros..."
             className="border border-gray-300 rounded-lg p-2 flex-fill"
           />
           <button type="submit" className="btn btn-dark rounded-lg p-2">
             Buscar
           </button>
-          <button onClick={() => {
-            setQuery('');
-            setBooks([]);
-
-          }}>
+          <button
+          className="btn btn-dark rounded-lg p-2"
+            type="button"
+            onClick={() => {
+              setQuery("");
+              setBooks([]);
+            }}
+          >
             Reiniciar
           </button>
         </form>
-        </div>
-        <div className="d-flex flex-row flex-wrap justify-content-center my-4 gap-3">
-          {books.length > 0 ? (
-            books.map((item) => (
-              <BookCard key={item.id} book={item.volumeInfo} />
-            ))
-          ) : (
-            <main className="p-2 col-12 col-md-6">
-            <p>
+      </div>
+      <div className="d-flex flex-row flex-wrap justify-content-center my-4 gap-3">
+        {books.length > 0 ? (
+          books.map((item) => <BookCard key={item.id} book={item.volumeInfo} />)
+        ) : (
+          <main className="p-2 col-12 col-md-6">
+            {errorMsg && <p className="text-danger">{errorMsg}</p>}
+            <p >
               BookBuzz es un frontend ligero desarrollado con React, Vite y
               Bootstrap que permite a los usuarios buscar libros de manera
               rápida y visual. La aplicación se conecta a la API pública de
               Google Books, mostrando los resultados en tarjetas interactivas.
-              </p>
-              <p>
+            </p>
+            <p>
               Su diseño modular y basado en componentes React permite una
               experiencia de usuario fluida y escalable, ideal para quienes
               quieren explorar libros de forma intuitiva.
-              </p> 
-              <p>Si quieres ver el
-              código completo, explorar la estructura del proyecto o probarlo
-              por tu cuenta, puedes visitar mi repositorio en GitHub
-              </p>
-              <div className="d-flex gap-3">
-              <button><a href="https://github.com/SkynoDore/book-finder/">Repositorio</a></button>
-              <button><a href="https://skynodore.github.io/">Mi portafolio</a></button>
-              </div>
-              
-              
-            </main>
-          )}
-        </div>
-      
+            </p>
+            <p>
+              Si quieres ver el código completo, explorar la estructura del
+              proyecto o probarlo por tu cuenta, puedes visitar mi repositorio
+              en GitHub
+            </p>
+            <div className="d-flex gap-3">
+              <button type="button" className="btn btn-dark rounded-lg p-2">
+                <a href="https://github.com/SkynoDore/book-finder/">
+                  Repositorio
+                </a>
+              </button>
+              <button type="button" className="btn btn-dark rounded-lg p-2">
+                <a href="https://skynodore.github.io/">Mi portafolio</a>
+              </button>
+            </div>
+          </main>
+        )}
+      </div>
     </>
   );
 }
